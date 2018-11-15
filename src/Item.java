88eafa20f4bdbe0356d1057109cfa0fceba3b2c1
zenailde.alexandre <zenailde.alexandre@JOIC05PC006.catolicasc.org.br>
2018-11-14 22:02:48 -0200

    Inserir classe item

---
+++ src/Item.java
@@ -0,0 +1,34 @@
+
+public class Item {
+	
+	private double peso;
+	private String desc;
+	
+	
+
+	public Item(String desc, double peso) {
+		super();
+		this.peso = peso;
+		this.desc = desc;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+	
+
+}
